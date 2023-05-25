eaba4bb527449d1341db20f449d5462f7c703d6f
fosterguys <62483900+igordantasgf@users.noreply.github.com>
2023-05-25 20:16:27 -0300

    Add files via upload

---
+++ ufba_engsoft1/atividade_1/BancoDados (1).java
@@ -0,0 +1,17 @@
+//Princípio aberto-fechado
+
+abstract class Storage{
+  public abstract void save(int ano, int mes, double valor);
+}
+
+class store_BancoDeDados extends Storage{
+  public override void save(int ano, int mes, double valor){
+    System.out.println("Simulando o registro em banco de dados: " + ano + "/" + mes + " " + valor);
+  }
+}
+
+class store_Arquivo extends Storage{
+  public override void save(int ano, int mes, double valor){
+    System.out.println("Simulando o registro em arquivo: " + ano + "/" + mes + " " + valor);
+  }
+}
